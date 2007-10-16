660346feed241a68829d1c677d93345d37b38a7b
jaybinks <jaybinks@b956fd51-792f-4845-bead-9b4dfca2ff2c>
2007-10-16 11:11:30 +0000

    fixed bamboo build script to recompile Resource... oops
    git-svn-id: svn://svn.code.sf.net/p/ultrastardx/svn/branches/1.01@514 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- Game/Code/bamboo-build-win-delphi.bat
+++ Game/Code/bamboo-build-win-delphi.bat
@@ -1,3 +1,6 @@
+del UltraStar.res
+"C:\Program Files\Borland\BDS\4.0\Bin\BRC32 -r UltraStar.RC
+
 "C:\Program Files\Borland\BDS\4.0\Bin\dcc32.exe" -U"lib\JEDI-SDLv1.0\SDL\Pas" -O"lib\JEDI-SDLv1.0\SDL\Pas" -I"lib\JEDI-SDLv1.0\SDL\Pas" -R"lib\JEDI-SDLv1.0\SDL\Pas" UltraStar.dpr
 copy UltraStar.exe ..\..\
 cd ..\..\
