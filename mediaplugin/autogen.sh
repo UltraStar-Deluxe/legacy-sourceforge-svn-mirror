6399f4f1ba3ecca8fe995b7825da1d4eb34773ef
tobigun <tobigun@b956fd51-792f-4845-bead-9b4dfca2ff2c>
2010-11-10 15:21:26 +0000

    removed call to libtoolize which seems to be unnecessary
    git-svn-id: svn://svn.code.sf.net/p/ultrastardx/svn/branches/experimental@2724 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- mediaplugin/autogen.sh
+++ mediaplugin/autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 AUTOGEN_DIR=dists/autogen
+#libtoolize
 aclocal -I ${AUTOGEN_DIR}/m4 && autoconf
 cd src/plugins/media/
 ./autogen.sh
--- mediaplugin/src/plugins/media/ffmpeg/autogen.sh
+++ mediaplugin/src/plugins/media/ffmpeg/autogen.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 AUTOGEN_DIR=../../../../dists/autogen
-libtoolize
 aclocal -I ${AUTOGEN_DIR}/m4 && autoconf
 automake -a
 
