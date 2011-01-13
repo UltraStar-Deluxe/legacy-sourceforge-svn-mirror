9a4397bc11b285bc09c32f73d7e41ae656790da4
tobigun <tobigun@b956fd51-792f-4845-bead-9b4dfca2ff2c>
2011-01-13 15:55:02 +0000

    buildbot script: fix relative path
    git-svn-id: svn://svn.code.sf.net/p/ultrastardx/svn/branches/experimental@2793 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- mediaplugin/dists/buildbot/win32-delphi-build.sh
+++ mediaplugin/dists/buildbot/win32-delphi-build.sh
@@ -1,3 +1,3 @@
-cd ../../src/mediaplugins/ffmpeg
+cd src/mediaplugins/ffmpeg
 ./configure ffmpeg_CFLAGS="-Ilibffmpeg/include" ffmpeg_LIBS="-Llibffmpeg/lib -lavcodec -lavformat -lavutil" libswscale_CFLAGS="-Ilibffmpeg/include" libswscale_LIBS="-Llibffmpeg/lib -lswscale"
 make compile
