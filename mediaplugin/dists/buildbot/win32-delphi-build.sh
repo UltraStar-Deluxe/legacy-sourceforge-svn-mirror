15e63900a1c3f8a9bc4b9a8d13eedb49df5b0785
tobigun <tobigun@b956fd51-792f-4845-bead-9b4dfca2ff2c>
2011-01-13 15:59:40 +0000

    buildbot: autogen.sh added
    git-svn-id: svn://svn.code.sf.net/p/ultrastardx/svn/branches/experimental@2794 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- mediaplugin/dists/buildbot/win32-delphi-build.sh
+++ mediaplugin/dists/buildbot/win32-delphi-build.sh
@@ -1,3 +1,4 @@
 cd src/mediaplugins/ffmpeg
+./autogen.sh
 ./configure ffmpeg_CFLAGS="-Ilibffmpeg/include" ffmpeg_LIBS="-Llibffmpeg/lib -lavcodec -lavformat -lavutil" libswscale_CFLAGS="-Ilibffmpeg/include" libswscale_LIBS="-Llibffmpeg/lib -lswscale"
 make compile
