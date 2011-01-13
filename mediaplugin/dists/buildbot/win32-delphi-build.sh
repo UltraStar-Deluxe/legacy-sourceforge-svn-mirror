67d7b72a4d1af427338c858ab5922fe967f3de92
tobigun <tobigun@b956fd51-792f-4845-bead-9b4dfca2ff2c>
2011-01-13 18:23:19 +0000

    check for correct PATH environment var add FFMPEG_LIBDIR var
    git-svn-id: svn://svn.code.sf.net/p/ultrastardx/svn/branches/experimental@2796 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- mediaplugin/dists/buildbot/win32-delphi-build.sh
+++ mediaplugin/dists/buildbot/win32-delphi-build.sh
@@ -1,4 +1,12 @@
+# check if the MinGW/Msys grep is used and not Borland's or FPC's version
+GREP_DIR=`which grep`
+if [ "$GREP_DIR" != "/bin/grep.exe" ]; then
+  echo "Incorrect version of grep ($GREP_DIR)"
+  echo "Make sure mingw/msys precede FPC and Delphi in the PATH environment."
+  exit 1
+fi
 cd src/mediaplugins/ffmpeg
 ./autogen.sh
-./configure ffmpeg_CFLAGS="-Ilibffmpeg/include" ffmpeg_LIBS="-Llibffmpeg/lib -lavcodec -lavformat -lavutil" libswscale_CFLAGS="-Ilibffmpeg/include" libswscale_LIBS="-Llibffmpeg/lib -lswscale"
+FFMPEG_LIBDIR="libffmpeg"
+./configure ffmpeg_CFLAGS="-I${FFMPEG_LIBDIR}/include" ffmpeg_LIBS="-L${FFMPEG_LIBDIR}/lib -lavcodec -lavformat -lavutil" libswscale_CFLAGS="-I${FFMPEG_LIBDIR}/include" libswscale_LIBS="-L${FFMPEG_LIBDIR}/lib -lswscale"
 make compile
