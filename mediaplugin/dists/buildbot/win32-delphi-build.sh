c9855e38aefa03b163938448bdd6c04183a981d3
tobigun <tobigun@b956fd51-792f-4845-bead-9b4dfca2ff2c>
2011-01-14 09:17:19 +0000

    allow /usr/bin/grep.exe
    git-svn-id: svn://svn.code.sf.net/p/ultrastardx/svn/branches/experimental@2797 b956fd51-792f-4845-bead-9b4dfca2ff2c

--- mediaplugin/dists/buildbot/win32-delphi-build.sh
+++ mediaplugin/dists/buildbot/win32-delphi-build.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 # check if the MinGW/Msys grep is used and not Borland's or FPC's version
 GREP_DIR=`which grep`
-if [ "$GREP_DIR" != "/bin/grep.exe" ]; then
+if ([ "$GREP_DIR" != "/bin/grep.exe" ] && [ "$GREP_DIR" != "/usr/bin/grep.exe" ]); then
   echo "Incorrect version of grep ($GREP_DIR)"
   echo "Make sure mingw/msys precede FPC and Delphi in the PATH environment."
   exit 1
